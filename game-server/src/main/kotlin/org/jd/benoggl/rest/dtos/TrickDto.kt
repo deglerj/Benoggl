223cae78510221cd6afed419caf7483cfb1fd8f6
Johannes Degler <johannes@degler-online.de>
2020-12-30 18:27:53 +0100

    Adds missing DTOs

---
+++ game-server/src/main/kotlin/org/jd/benoggl/rest/dtos/TrickDto.kt
@@ -0,0 +1,21 @@
+package org.jd.benoggl.rest.dtos
+
+import org.jd.benoggl.model.TrickState
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+
+data class TrickDto(
+
+    @get:NotNull
+    @get:Min(0)
+    val number: Int?,
+
+    @get:NotNull
+    val state: TrickState?,
+
+    @get:NotNull
+    val pendingPlayerUids: List<String>?,
+
+    val winnerUid: String?
+) {
+}
