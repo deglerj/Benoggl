3308b5a74f9ea03cf1f85a863d498c82c33b287b
Johannes Degler <johannes@degler-online.de>
2021-01-04 17:38:19 +0100

    Implements dealing cards

--- game-server/src/main/kotlin/org/jd/benoggl/services/CardService.kt
+++ game-server/src/main/kotlin/org/jd/benoggl/services/CardService.kt
@@ -9,16 +9,32 @@ class CardService {
     fun dealCards(playerCount: Int): DealtCards {
         check(playerCount >= 2)
 
-        // TODO implement
-        val card = Card(Suit.ACORNS, Rank.ACE)
-        val playerHands = mutableListOf<Hand>()
-        repeat(playerCount) {
-            playerHands.add(Hand(mutableListOf(card), HandType.NORMAL))
-        }
-        return DealtCards(
-            playerHands,
-            Hand(mutableListOf(card), HandType.DABB)
-        )
+        val deck = createDeck().shuffled().toMutableList()
+        val dabb = takeDabbCards(deck)
+        val playerHands = splitDeckBetweenPlayers(deck, playerCount)
+
+        return DealtCards(playerHands, dabb)
     }
 
+    private fun takeDabbCards(deck: MutableList<Card>): Hand {
+        val cards = (1..4).map { deck.removeAt(0) }
+        return Hand(cards.toMutableList(), HandType.DABB)
+    }
+
+    private fun splitDeckBetweenPlayers(deck: MutableList<Card>, playerCount: Int): List<Hand> {
+        val cardsPerPlayer = deck.size / playerCount
+        return deck.chunked(cardsPerPlayer) { Hand(it.toMutableList(), HandType.NORMAL) }
+    }
+
+
+    private fun createDeck(): List<Card> {
+        return Suit.values().flatMap { suit ->
+            Rank.values().flatMap { rank ->
+                listOf(
+                    Card(suit, rank),
+                    Card(suit, rank)
+                )
+            }
+        }
+    }
 }
---
+++ game-server/src/test/kotlin/org/jd/benoggl/services/CardServiceTest.kt
@@ -0,0 +1,27 @@
+package org.jd.benoggl.services
+
+import io.quarkus.test.junit.QuarkusTest
+import org.jd.benoggl.model.HandType
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import javax.enterprise.inject.Default
+import javax.inject.Inject
+
+@QuarkusTest
+class CardServiceTest {
+
+    @Inject
+    @field: Default
+    internal lateinit var sut: CardService
+
+    @Test
+    fun dealCards() {
+        val dealt = sut.dealCards(2)
+
+        assertEquals(4, dealt.dabb.cards.size)
+        assertEquals(HandType.DABB, dealt.dabb.type)
+        assertEquals(2, dealt.playerHands.size)
+        assertEquals((40 - 4) / 2, dealt.playerHands[0].cards.size)
+        assertEquals(HandType.NORMAL, dealt.playerHands[0].type)
+    }
+}
